f7d6d5b75a889fda3fb63a21a7a3894641910a52
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-05-03 11:50:02 +0200

    first commit

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,23 @@
+export const convertPLNToUSD = (PLN) => {
+  if (typeof PLN === "string" || PLN instanceof String) {
+    return NaN;
+  } else if (PLN === undefined) {
+    return NaN;
+  } else if (
+    (typeof PLN !== "string" || PLN instanceof String) &&
+    typeof PLN !== "number"
+  ) {
+    return "Error";
+  } else if (PLN < 0) {
+    return "$0.00";
+  } else {
+    const PLNtoUSD = PLN / 3.5;
+
+    const formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, " ");
+  }
+};
