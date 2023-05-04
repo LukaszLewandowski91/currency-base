9b164b3dae95489f395e215a5ec66cef6fde881c
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-05-04 21:12:45 +0200

    fix if loop in convertPLNToUSD.js

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,23 +1,22 @@
 export const convertPLNToUSD = (PLN) => {
   if (typeof PLN === "string" || PLN instanceof String) {
     return NaN;
-  } else if (PLN === undefined) {
-    return NaN;
-  } else if (
-    (typeof PLN !== "string" || PLN instanceof String) &&
-    typeof PLN !== "number"
-  ) {
-    return "Error";
-  } else if (PLN < 0) {
-    return "$0.00";
-  } else {
-    const PLNtoUSD = PLN / 3.5;
-
-    const formatter = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    });
-
-    return formatter.format(PLNtoUSD).replace(/\u00a0/g, " ");
   }
+  if (PLN === undefined) {
+    return NaN;
+  }
+  if (typeof PLN !== "number") {
+    return "Error";
+  }
+  if (PLN < 0) {
+    return "$0.00";
+  }
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, " ");
 };
